d298bf6b74d49bce4e70dd9f6b13ef6ae8006957
wibu-elite <miftahurrohman100@gmail.com>
2023-10-08 22:07:15 +0700

    Menambahkan deklarasi variabel di file hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	var anggotaSatu string = "Key"
+
+	// var anggotaDua string
+	// anggotaDua = "Ken"
+
+	anggotaTiga := "Joy"
+
 	fmt.Println("Hello World")
+	fmt.Println(anggotaSatu, "\n", anggotaTiga)
 
 }
