29a05beb2a34ac38fc423c311223691491e1b64d
wibu-elite <miftahurrohman100@gmail.com>
2023-10-09 09:24:50 +0700

    Menambahkan Baris Inputan ke dalam File hello-wolrd.go

--- hello-world.go
+++ hello-world.go
@@ -12,6 +12,12 @@ func main() {
 
 	timSatu, timDua, timTiga := "satu", "dua", "tiga"
 
+	var inputSatu string
+
+	fmt.Print("Input Masukan Pertama:")
+	fmt.Scan(&inputSatu)
+	fmt.Println("Your First Input is : ", inputSatu)
+
 	fmt.Println("Hello World")
 	fmt.Println(anggotaSatu, anggotaTiga)
 	fmt.Println(timSatu, timDua, timTiga)
