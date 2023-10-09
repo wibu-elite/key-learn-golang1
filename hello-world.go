efe76ac19d686cf59b6e940071a35baeddad1585
wibu-elite <miftahurrohman100@gmail.com>
2023-10-09 20:59:17 +0700

    menambahkan deklarasi variabel denga new, dan menambahkan type data bool

--- hello-world.go
+++ hello-world.go
@@ -14,6 +14,14 @@ func main() {
 
 	var inputSatu string
 
+	myName := new(string)
+
+	var try bool = true
+
+	fmt.Printf("try? %t", try)
+
+	fmt.Println(*myName)
+
 	fmt.Print("Input Masukan Pertama:")
 	fmt.Scan(&inputSatu)
 	fmt.Println("Your First Input is : ", inputSatu)
